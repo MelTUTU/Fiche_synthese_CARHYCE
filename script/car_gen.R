dc36c7446d697f7e4bac43ae2a5f2d5d48778bba
MelTUTU <melanie.tual@ofb.gouv.fr>
2024-02-29 12:36:55 +0100

    et toujours plus !

--- script/car_gen.R
+++ script/car_gen.R
@@ -22,11 +22,20 @@ operation <- read_delim("data_raw/export_csv_data-20240220142639/OPERATION.csv",
 str(operation)
 
 
-#chargement de la table point de pvmt pour récupérer le code station
+#chargement de la table pour récupérer les codes dpt
+station_mesure <- read_delim("data_raw/export_csv_data-20240220142639/STATION_MESURE.csv", 
+                             delim = ";", escape_double = FALSE, locale = locale(encoding = "WINDOWS-1252"), 
+                             trim_ws = TRUE)
+str(station_mesure)
+
+
+#chargement de la table point de pvmt pour récupérer le code departement
 
 pvmt <- read_delim("data_raw/export_csv_data-20240220142639/POINT_PRELEVEMENT.csv",
                    na=c("null",""))
 str(pvmt)
+
+
 
 #croisement pour rapatrier code sandre dans tableau de travail et récupérer date
 
@@ -37,10 +46,42 @@ ope_croise <- operation %>%
   mutate(annee=dmy(OPE_DATE_REALISATION),
          annee=year(annee))
 
-# par station,selectionner code opération le plus récent
-ope_recent <-ope_croise %>% 
+# croisement pour rapatrier code departement dans table operation
+ope_croise2 <-ope_croise %>% 
+  left_join(y=station_mesure %>% 
+              select(SME_CD_STATION_MESURE_EAUX_SURFACE:SME_COORD_Y_STATION_MESURE_EAUX_SURFACE))
+
+
+# par station,selectionner infos de l'opé la plus recente
+ope_recent <-ope_croise2 %>% 
   dplyr::group_by(SME_CD_STATION_MESURE_EAUX_SURFACE) %>% 
-  filter(last(annee))
+  filter(annee==max(annee))
+
+#renommer colonne (pour le fun!)
+nom<-colnames(ope_recent)
+nouveau_nom<-nom %>% 
+  str_replace_all(pattern = "\\(", replacement="") %>% # pas utile dans ce contexte mais c'est pour avoir la fonction ;)
+  str_to_lower()
+colnames(ope_recent)<-nouveau_nom
+
+
+#chargement de la table transect
+transect <- read_delim("data_raw/export_csv_data-20240220142639/TRANSECT.csv", 
+                       delim = ";", escape_double = FALSE, col_types = cols(TRA_LPB = col_number(), 
+                                                                            TRA_L_MOUILLEE = col_number(), TRA_HPB = col_number()), 
+                       locale = locale(encoding = "WINDOWS-1252"), 
+                       na = "empty", trim_ws = TRUE)
+str(transect)
+
+
+
+
+
+# sélection des transects correspondants aux opé les plus récentes
+transect_recent <- transect %>% 
+  left_join(y=ope_recent %>% 
+              select)
+
 
 
 
