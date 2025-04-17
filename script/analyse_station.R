f740cc4ee77228c0eaadff5e663f41ead15cb509
MelTUTU <melanie.tual@ofb.gouv.fr>
2025-04-17 11:30:55 +0200

    construction d'un tableau global avec analyse sur param structurants

--- script/analyse_station.R
+++ script/analyse_station.R
@@ -6,25 +6,31 @@
 library(ggplot2)
 library(wesanderson)
 
-#dans tableau transect, remplacer tous les -999 par NA pour les largeurs et hauteurs > à optimiser !
-Larg_haut_analyse<-transect %>%
-  dplyr::mutate (TRA_LPBmod=ifelse(TRA_LPB<0,'NA',TRA_LPB)) %>% 
-  dplyr::mutate (TRA_L_MOUILLEEmod=ifelse(TRA_L_MOUILLEE<0,'NA',TRA_L_MOUILLEE)) %>% 
-  dplyr::mutate (TRA_HPBmod=ifelse(TRA_HPB<0,'NA',TRA_HPB))
+#Remplacer par NA, directement dans mon tableau, toutes valeurs qui sont inférieures strictement à 0
+transect[transect<0]<-NA
 
-
-#calculer la largeur à plein bord moyenne, médiane, min, max > ca ca fonctionne pas :(
-#lpb_analyse<-Larg_haut_analyse %>% 
-#  dplyr::group_by (TRA_OPE_ID) %>%
-#  summarise(moy_lpb=mean(TRA_LPBmod,na.rm=TRUE),median_lpb=median(TRA_LPBmod, na.rm=TRUE),min_lpb=min(TRA_LPBmod, na.rm=TRUE),max_lpb=max(TRA_LPBmod,na.rm=TRUE))
-
-#calculer la largeur à plein bord moyenne, médiane, min, max > ca ca fonctionnait
+#calculer la largeur à plein bord moyenne, médiane, min, max
 lpb_analyse<-transect %>% 
   dplyr::group_by (TRA_OPE_ID) %>%
-  summarise(moy_lpb=mean(TRA_LPB),median_lpb=median(TRA_LPB),min_lpb=min(TRA_LPB),max_lpb=max(TRA_LPB))
+  summarise(moy_lpb=mean(TRA_LPB,na.rm=TRUE),median_lpb=median(TRA_LPB, na.rm=TRUE),min_lpb=min(TRA_LPB, na.rm=TRUE),max_lpb=max(TRA_LPB,na.rm=TRUE))
+
+#calculer la largeur mouillée moyenne, médiane, min, max
+lm_analyse <-transect %>% 
+  dplyr::group_by (TRA_OPE_ID) %>%
+  summarise(moy_lm=mean(TRA_L_MOUILLEE,na.rm=TRUE),median_lm=median(TRA_L_MOUILLEE, na.rm=TRUE),min_lm=min(TRA_L_MOUILLEE, na.rm=TRUE),max_lm=max(TRA_L_MOUILLEE,na.rm=TRUE))
+
+#calculer la hauteur pb moyenne, médiane, min, max
+Hpb_analyse<-transect %>% 
+  dplyr::group_by (TRA_OPE_ID) %>%
+  summarise(moy_Hpb=mean(TRA_HPB,na.rm=TRUE),median_Hpb=median(TRA_HPB, na.rm=TRUE),min_Hpb=min(TRA_HPB, na.rm=TRUE),max_Hpb=max(TRA_HPB,na.rm=TRUE))
+
+# Grouper tout ça dans un tableau
+tot_analyse<-dplyr::left_join(lpb_analyse,lm_analyse,by='TRA_OPE_ID')
+tot_analyse<-dplyr::left_join(tot_analyse,Hpb_analyse,by='TRA_OPE_ID')
 
 
-#représenter graphiquement les valeurs "structurantes" de la station
+
+#représenter graphiquement les valeurs "structurantes" de la station : exemple de la largeur à plein bord
 graph_lpb1 <-
   ggplot(data=lpb_analyse, aes(x=moy_lpb,y=TRA_OPE_ID))+
   geom_point(aes(color='coral'))  +
@@ -43,9 +49,41 @@ tab_court<-transect %>%
 tab_transfo<-tab_court %>% 
   pivot_longer(cols=-'TRA_ID',
                names_to="type",
-               values_to="valeur")%>% 
+               values_to="valeur")
 
-  # il faudrait rajouter le TRA_ID
+# il faudrait rajouter le TRA_ID
+#tab_transfo2<-tab_court %>% 
+#  left_join(y=transect$TRA_OPE_ID,by="TRA_ID") marche pas !
+
+ 
+graphlpb2<-ggplot(data=tab_transfo)+
+  geom_point(aes(x=valeur,y=TRA_ID,color=type))+
+  geom_segment(aes(x=0,xend=valeur,y=TRA_ID,yend=TRA_ID),color='blue')
+
+graphlpb2  
+
+
+
+# Change baseline
+ggplot(data, aes(x=x, y=y)) +
+  geom_segment( aes(x=x, xend=x, y=1, yend=y), color="grey") +
+  geom_point( color="orange", size=4) +
+  theme_light() +
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.border = element_blank(),
+    axis.ticks.x = element_blank()
+  ) +
+  xlab("") +
+  ylab("Value of Y")
+
+
+  
+  
+  
+ 
+  
+  
 #graphique > marche pas car je n'arrive pas à installer la librairie qu'il faut
 #library(streamgraph)
 #graph_1<-streamgraph(tab_transfo,key='type',value='valeur',date='TRA_ID',
